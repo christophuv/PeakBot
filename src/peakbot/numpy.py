*fixed: many bugs (generation of training examples, plotting, gradient-descend preprocessing) *updated: python3.8

src/peakbot/numpy.py
@@ -9,6 +9,7 @@ import scipy.stats
 
 import math
 import platform
+import os
 
 @timeit
 def initializeNUMPYFunctions(rtSlices = None, mzSlices = None, eicWindowPlusMinus = 30, SavitzkyGolayWindowPlusMinus = 5):
@@ -793,10 +794,10 @@ def exportForPeakBot(mzxml, pathTo, expList, filterLine, rtSlices = None, mzSlic
         with sharedLock:
             outVal = sharedValue.value
             sharedValue.value += 1
-        pickle.dump([datAll, propertiesAll], open("%s/sample%d.pickle"%(pathTo, outVal), "wb"))
+        pickle.dump([datAll, propertiesAll], open(os.path.join("%s", "sample%d.pickle"%(pathTo, outVal)), "wb"))
         if verbose and False:
             with sharedPrintLock:
-                print(" .. exported %d local maxima to file %s"%(datAll.shape[0], "%s/sample%d.pickle"%(pathTo, outVal)))
+                print(" .. exported %d local maxima to file %s"%(datAll.shape[0], os.path.join("%s", "sample%d.pickle"%(pathTo, outVal))))
         datAll = None
         propertiesAll = None
         packNum += 1

src/peakbot/train/core.py
@@ -11,56 +11,61 @@ import random
 import numpy as np
 
 
-def shuffleResultsSampleNames(exportPath, instancePrefix = None, verbose = False):
-    
+def shuffleResultsSampleNames(exportPath, instancePrefix=None,
+                              tempFileName="bqu40mcb25473zfhbgwh22534", verbose=False):
+
     if instancePrefix is None:
         instancePrefix = peakbot.Config.INSTANCEPREFIX
-    
+
     tic("shuffling")
     if verbose:
         print("Shuffling the test instances (batch name shuffling)")
-    files = [os.path.join(exportPath, f) for f in os.listdir(exportPath) if os.path.isfile(os.path.join(exportPath, f))]
-    if verbose: 
-        print("  | .. there are %d files"%(len(files)))
-    
+    files = [os.path.join(exportPath, f) for f in os.listdir(
+        exportPath) if os.path.isfile(os.path.join(exportPath, f))]
+    if verbose:
+        print("  | .. there are %d files" % (len(files)))
+
     unused = [t for t in range(len(files))]
     random.shuffle(unused)
     for i in range(len(files)):
         j = unused[0]
         del unused[0]
-        
-        os.rename(files[i], os.path.join(pathlib.Path(files[i]).parent.resolve(), "temp%d.pickle"%i))
-    
-    files = [os.path.join(exportPath, f) for f in os.listdir(exportPath) if os.path.isfile(os.path.join(exportPath, f))]
-    for i in range(len(files)):
-        os.rename(files[i], files[i].replace("temp", instancePrefix))
-        
-    if verbose:
-        print("  | .. took %.1f seconds"%toc("shuffling"))
-        print("")
-    
 
-        
-def shuffleResults(exportPath, steps = 1E5, samplesToExchange = 50, instancePrefix = None, verbose = False):
-    
+        os.rename(files[i], os.path.join(pathlib.Path(
+            files[i]).parent.resolve(), "%s%d.pickle" % (tempFileName, i)))
+
+    files = [os.path.join(exportPath, f) for f in os.listdir(
+        exportPath) if os.path.isfile(os.path.join(exportPath, f))]
+    for i in range(len(files)):
+        os.rename(files[i], files[i].replace(tempFileName, instancePrefix))
+
+    if verbose:
+        print("  | .. took %.1f seconds" % toc("shuffling"))
+        print("")
+
+
+def shuffleResults(exportPath, steps=1E5, samplesToExchange=50,
+                   instancePrefix=None, verbose=False):
+
     if instancePrefix is None:
         instancePrefix = peakbot.Config.INSTANCEPREFIX
-        
+
     tic("shuffling")
     if verbose:
         print("Shuffling the test instances (inter-batch shuffling)")
-    files = [os.path.join(exportPath, f) for f in os.listdir(exportPath) if os.path.isfile(os.path.join(exportPath, f))]
-    if verbose: 
-        print("  | .. there are %d files"%(len(files)))
-    
-    with tqdm.tqdm(total = steps, desc="  | .. shuffling", disable=not verbose) as t:
+    files = [os.path.join(exportPath, f) for f in os.listdir(
+        exportPath) if os.path.isfile(os.path.join(exportPath, f))]
+    if verbose:
+        print("  | .. there are %d files" % (len(files)))
+
+    with tqdm.tqdm(total=steps, desc="  | .. shuffling", disable=not verbose) as t:
         while steps > 0:
-            filea = files[random.randint(0, len(files)-1)]
-            fileb = files[random.randint(0, len(files)-1)]
+            filea = files[random.randint(0, len(files) - 1)]
+            fileb = files[random.randint(0, len(files) - 1)]
 
             if filea == fileb:
                 continue
-            
+
             with open(filea, "rb") as temp:
                 a = pickle.load(temp)
             with open(fileb, "rb") as temp:
@@ -68,35 +73,39 @@ def shuffleResults(exportPath, steps = 1E5, samplesToExchange = 50, instancePref
 
             samplesA = a["LCHRMSArea"].shape[0]
             samplesB = b["LCHRMSArea"].shape[0]
-            
+
             cExchange = min(min(samplesA, samplesB), samplesToExchange)
-            
+
             beginA = random.randint(0, samplesA - cExchange)
             beginB = random.randint(0, samplesB - cExchange)
-            
+
             for k in a.keys():
-                if isinstance(a[k], np.ndarray) and len(a[k].shape)==2:
-                    temp = a[k][beginA:(beginA+cExchange),:]
-                    a[k][beginA:(beginA+cExchange),:] = b[k][beginB:(beginB+cExchange),:]
-                    b[k][beginB:(beginB+cExchange),:] = temp
-                    
-                elif isinstance(a[k], np.ndarray) and len(a[k].shape)==3:
-                    temp = a[k][beginA:(beginA+cExchange),:,:]
-                    a[k][beginA:(beginA+cExchange),:,:] = b[k][beginB:(beginB+cExchange),:,:]
-                    b[k][beginB:(beginB+cExchange),:,:] = temp
-                    
-                elif isinstance(a[k], np.ndarray) and len(a[k].shape)==4:
-                    temp = a[k][beginA:(beginA+cExchange),:,:,:]
-                    a[k][beginA:(beginA+cExchange),:,:,:] = b[k][beginB:(beginB+cExchange),:,:,:]
-                    b[k][beginB:(beginB+cExchange),:,:,:] = temp
-                
+                if isinstance(a[k], np.ndarray) and len(a[k].shape) == 2:
+                    temp = a[k][beginA:(beginA + cExchange), :]
+                    a[k][beginA:(beginA + cExchange),
+                         :] = b[k][beginB:(beginB + cExchange), :]
+                    b[k][beginB:(beginB + cExchange), :] = temp
+
+                elif isinstance(a[k], np.ndarray) and len(a[k].shape) == 3:
+                    temp = a[k][beginA:(beginA + cExchange), :, :]
+                    a[k][beginA:(beginA + cExchange), :,
+                         :] = b[k][beginB:(beginB + cExchange), :, :]
+                    b[k][beginB:(beginB + cExchange), :, :] = temp
+
+                elif isinstance(a[k], np.ndarray) and len(a[k].shape) == 4:
+                    temp = a[k][beginA:(beginA + cExchange), :, :, :]
+                    a[k][beginA:(beginA + cExchange), :, :,
+                         :] = b[k][beginB:(beginB + cExchange), :, :, :]
+                    b[k][beginB:(beginB + cExchange), :, :, :] = temp
+
                 elif isinstance(a[k], list):
-                    temp = a[k][beginA:(beginA+cExchange)]
-                    a[k][beginA:(beginA+cExchange)] = b[k][beginB:(beginB+cExchange)]
-                    b[k][beginB:(beginB+cExchange)] = temp
-            
+                    temp = a[k][beginA:(beginA + cExchange)]
+                    a[k][beginA:(beginA + cExchange)
+                         ] = b[k][beginB:(beginB + cExchange)]
+                    b[k][beginB:(beginB + cExchange)] = temp
+
             assert samplesA == a["LCHRMSArea"].shape[0] and samplesB == b["LCHRMSArea"].shape[0]
-            
+
             with open(filea, "wb") as temp:
                 pickle.dump(a, temp)
             with open(fileb, "wb") as temp:
@@ -104,8 +113,7 @@ def shuffleResults(exportPath, steps = 1E5, samplesToExchange = 50, instancePref
 
             steps = steps - 1
             t.update()
-        
+
     if verbose:
-        print("  | .. took %.1f seconds"%toc("shuffling"))
+        print("  | .. took %.1f seconds" % toc("shuffling"))
         print("")
-    